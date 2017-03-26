5f8bf8830906cc566bb10a141601d46022fba283
arhillis <andriellerhillis@gmail.com>
2017-03-26 18:33:47 -0500

    Using JavaScript to display summaries for each of my projects. Also changed the IDs around to be for the entire project div and not just the image. The changes in the stylesheet reflect this.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-var projectEls = document.getElementsByClassName("project");
+var sumEl = document.getElementById("summary");
 
-for(var i = 0; i < projectEls.length; i++){
-    function mOver(projectEls[i]){
-        
-    }
+document.getElementById("tribute").addEventListener("mouseover", function() {
+    sumEl.innerHTML = "This is my tribute to King George VI that I wrote for the Free Code Camp curriculum.";
+});
+            
+document.getElementById("quotes").addEventListener("mouseover", function() {
+    sumEl.innerHTML = "This is the random quote generator that I wrote for the Free Code Camp curriculum.";
+});
+            
+var btnEls = document.getElementsByClassName("project");
+
+for(var i = 0; i < btnEls.length; i++){
+    btnEls[i].addEventListener("mouseleave", function() {
+        document.getElementById("summary").innerHTML = "Hover over a project to see a description.";
+    });
 }
