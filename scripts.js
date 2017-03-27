7ac2db56ea9c35a41d83fcde6779876d6334bd74
arhillis <andriellerhillis@gmail.com>
2017-03-27 08:45:59 -0500

    Added a 'blank' class to the blank project divs and a special message in the JavaScript file. I've also been trying to get the summary paragraph left aligned.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,14 @@ document.getElementById("tribute").addEventListener("mouseover", function() {
 document.getElementById("quotes").addEventListener("mouseover", function() {
     sumEl.innerHTML = "This is the random quote generator that I wrote for the Free Code Camp curriculum.";
 });
+
+var blankEls = document.getElementsByClassName("blank");
+
+for(var i = 0; i < blankEls.length; i++){
+    blankEls[i].addEventListener("mouseover", function() {
+        document.getElementById("summary").innerHTML = "Another exciting project comming soon.";
+    });
+}
             
 var btnEls = document.getElementsByClassName("project");
 
