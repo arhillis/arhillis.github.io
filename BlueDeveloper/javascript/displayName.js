ee1ae5d68cfa3c7e5378b0a34f3577aaf3211b39
arhillis <andriellerhillis@gmail.com>
2017-08-07 14:34:13 -0500

    Adding the Blue Developers stuff to test appcache

---
+++ BlueDeveloper/javascript/displayName.js
@@ -0,0 +1,63 @@
+/*
+To-do list:
+
+1. figure out how to get it to display the email address in the newsletter form.
+2. Add the clear button.
+
+
+*/
+
+var nameEl = document.getElementById("nameBox");
+var emailEl = document.getElementById("emailBox");
+var nameDisplay = document.getElementById("nameDisplay");
+var emailDisplay = document.getElementById("emailDisplay");
+var sendBtn = document.getElementById("sendBtn");
+var clearBtn = document.getElementById("clearBtn");
+
+function init(){
+    displayInfo();
+    sendBtn.addEventListener("click", saveInfo, false);
+    clearBtn.addEventListener("click", clearAll, false);
+}
+
+function saveInfo(){
+    saveEmail();
+    saveName();
+    alert("Name: " + localStorage.getItem("name") + "\nEmail: " + localStorage.getItem("email"));
+    displayInfo();
+}
+function displayInfo(){   
+    if(localStorage.getItem("email") == null){
+        emailDisplay.textContent = "Please enter an email address: ";
+    }else{
+        emailDisplay.innerHTML = (localStorage.getItem("email").length == 0)
+            ?      "Please enter an email address:"
+            :      "Current Email Address: \n" + localStorage.getItem("email");
+    }
+    
+    if(localStorage.getItem("name") == null){
+        nameDisplay.innerHTML = "Welcome back, User!";
+    }else{
+        nameDisplay.innerHTML = (localStorage.getItem("name").length == 0)
+            ?       "Welcome back, User!"
+            :       "Welcome back, " + localStorage.getItem("name") + "!";
+    }
+    
+}
+function saveName(){
+    localStorage.setItem("name", nameEl.value);
+}
+
+function saveEmail(){
+    localStorage.setItem("email", emailEl.value);
+}
+
+function clearAll(){
+    localStorage.clear();
+    nameEl.value = null;
+    emailEl.value = null;
+    nameDisplay.textContent = "Welcome back, User!";
+    emailDisplay.textContent = "Please enter an email address: ";
+}
+
+window.addEventListener("load", init(), false);
