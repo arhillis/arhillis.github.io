f5b161f136dad2ed955565f1dd5afb34b9865960
arhillis <andriellerhillis@gmail.com>
2021-03-10 16:22:18 -0600

    Starting again from scratch

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,11 @@
 window.onload = function(){
-    const nav = document.querySelector('nav');
-    const box = document.querySelector('.box');
+    // const nav = document.querySelector('nav');
+    // const box = document.querySelector('.box');
 
-    box.onclick = function(){
-        nav.classList.toggle('close');
-        box.classList.toggle('close');
-    }
+    // box.onclick = function(){
+    //     nav.classList.toggle('close');
+    //     box.classList.toggle('close');
+    // }
+
+    console.log('hELLO WORLD');
 }
